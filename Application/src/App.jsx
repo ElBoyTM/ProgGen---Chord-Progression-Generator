f755323bcbe24a6467737dca7214ffa3be3eb4e8
ElBoyTM <nszalenski@gmail.com>
2025-03-12 16:52:36 -0400

    added simple code to App.jsx

---
+++ Application/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+    return (
+      <div>
+        <h1>PROGGEN - Chord Progression Generator</h1>
+        <button>Generate Progression</button>
+      </div>
+    );
+  }
+  
+  export default App;
+  
