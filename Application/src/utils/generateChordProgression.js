4fb356cfb9de4f6b1e72b4c44845c80a022a72e3
ElBoyTM <nszalenski@gmail.com>
2025-03-17 15:43:33 -0400

    centered text, added error catchers, and somewhat fixed generateChordProgression

--- Application/src/utils/generateChordProgression.js
+++ Application/src/utils/generateChordProgression.js
@@ -1,15 +1,15 @@
-import { chord, scale } from 'tonal';
+import { Scale, Chord } from 'tonal';
 
 const CHORD_TYPES = ['maj', 'min', 'dim', 'aug'];
 
 export function generateChordProgression(key = 'C', progressionLength = 4) {
-  const scaleNotes = scale.get(`${key} major`).notes;
+  const scaleNotes = Scale.get(`${key} major`).notes;
   const progression = [];
 
   for (let i = 0; i < progressionLength; i++) {
     const note = scaleNotes[Math.floor(Math.random() * scaleNotes.length)];
     const type = CHORD_TYPES[Math.floor(Math.random() * CHORD_TYPES.length)];
-    const fullChord = chord.getChord(type, note);
+    const fullChord = Chord.getChord(type, note);
 
     progression.push(fullChord.symbol || `${note}${type}`);
   }
